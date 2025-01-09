6b0d46a4b0231c37996c93fb0ee9e66b87db4c0b
IsraMariem <isrameryemth@gmail.com>
2025-01-09 20:29:42 +0100

    Last Changes

--- app/routes/recomm.py
+++ app/routes/recomm.py
@@ -23,28 +23,3 @@ def get_recommendations(user_id):
     return jsonify([{'content': rec.content} for rec in recommendations]), 200
 
 
-
-
-
-
-
-
-
-
-
-"""from flask import Blueprint, jsonify
-from app.recommendation_logic import generate_recommendations
-from app.models import Recommendation
-
-bp_recom= Blueprint('recommendations', __name__)
-
-@bp_recom.route('/recommendations/<int:user_id>', methods=['GET'])
-def get_recommendations(user_id):
-    # Generate recommendations for the user
-    generate_recommendations(user_id)
-
-    # Retrieve the user's recommendations from the database
-    recommendations = Recommendation.query.filter_by(user_id=user_id).all()
-
-    # Return recommendations as a JSON response
-    return jsonify([{'content': rec.content} for rec in recommendations])"""
